57661fe829692b65e998697cd27199fc297b0f6c
Bauersachs Johannes (HR TA ET EDU VT MUC) <Johannes.Bauersachs@infineon.com>
2024-09-30 15:39:44 +0200

    Adding the Vue JS Frontend and adding some features in the Backend.

--- backend/src/main/java/com/joba/backend/InventoryApplication.java
+++ backend/src/main/java/com/joba/backend/InventoryApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryApplication {
+	@Autowired
+	private
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name){
-		return String.format("Hello %s!", name);
 	}
 }
---
+++ backend/src/main/java/com/joba/backend/repository/ItemRepository.java
@@ -0,0 +1,3 @@
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
